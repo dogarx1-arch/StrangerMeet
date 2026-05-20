912e9d1257a98d1fae4eeedc52b4f0d698008ea5
dogarx1-arch <fankarx1@gmail.com>
2026-05-20 13:13:56 -0700

    Update stats.js

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const sessionManager = require('../socket/sessionManager')
+const matchmaking = require('../socket/matchmaking')
 
 router.get('/', (req, res) => {
-  const stats = sessionManager.getStats()
+  const stats = sessionManager.getStats(matchmaking.getWaitingCount())
   res.json(stats)
 })
 
